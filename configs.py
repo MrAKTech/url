53e1ddd666068d274321eca73dbc01aaa4305db7
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2024-06-14 10:08:46 +0500

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,24 +1,54 @@
-import os
+import re, logging
+from os import environ
+from Script import script
+from dotenv import load_dotenv
+load_dotenv()
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 class Config(object):
   API_ID = int(os.environ.get("API_ID", "25435105"))
   API_HASH = os.environ.get("API_HASH", "011126265844f2d7cc7dc1a024f6bc78")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "6734734174:AAERH1lMzFq2dgAi8V-Fp0WLU18_yl4OMCI")
+  ANUSHKA_BOT_TOKEN = os.environ.get("ANUSHKA_BOT_TOKEN", "6734734174:AAERH1lMzFq2dgAi8V-Fp0WLU18_yl4OMCI")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "fileeestorebot")
+  ANUSHKABOT_USERNAME = os.environ.get("ANUSHKA_BOT_USERNAME", "fileeestorebot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002053044713"))
+  ANUSHKA_DB_CHANNEL = int(os.environ.get("ANUSHKA_DB_CHANNEL", "-1002053044713"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "ez4short.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "ade16f386d69aa3d86d6320b5b2c906996a8d2a6")
+  ANUSHKA_SHORTLINK_URL = os.environ.get('ANUSHKA_SHORTLINK_URL', "ez4short.com")
+  ANUSHKA_SHORTLINK_API = os.environ.get('ANUSHKA_SHORTLINK_API', "ade16f386d69aa3d86d6320b5b2c906996a8d2a6")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6459102722"))
+  ANUSHKA_BOT_OWNER = int(os.environ.get("ANUSHKA_BOT_OWNER", "6459102722"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SINGER:SINGER@cluster0.1dt9hoe.mongodb.net/?retryWrites=true&w=majority")
+  ANUSHKA_DATABASE_URL = os.environ.get("ANUSHKA_DATABASE_URL", "mongodb+srv://SINGER:SINGER@cluster0.1dt9hoe.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
   FILE_TO_LINK_APPURL = os.environ.get("FILE_TO_LINK_APPURL", "https://liuuqfilestreampro-278216c9f3ab.herokuapp.com")
   FILE_TO_LINK_LOG = int(os.environ.get("FILE_TO_LINK_LOG", "-1001991880018"))
+  ANUSHKA_UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+  ANUSHKA_FILE_TO_LINK_APPURL = os.environ.get("FILE_TO_LINK_APPURL", "https://liuuqfilestreampro-278216c9f3ab.herokuapp.com")
+  ANUSHKA_FILE_TO_LINK_LOG = int(os.environ.get("FILE_TO_LINK_LOG", "-1001991880018"))
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001956959333"))
+  ANUSHKALOG_CHANNEL = int(os.environ.get("ANUSHKA_LOG_CHANNEL", "-1001956959333"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  ANUSHKA_BANNED_USERS = set(int(x) for x in os.environ.get("ANUSHKA_BANNED_USERS", "").split())
+  ANUSHKA_FORWARD_AS_COPY = bool(os.environ.get("ANUSHKA__FORWARD_AS_COPY", True))
+  ANUSHKA_BROADCAST_AS_COPY = bool(os.environ.get("ANUSHKA_BROADCAST_AS_COPY", True))
+  ANUSHKA_BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  ANUSHKA_OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("ANUSHKA_OTHER_USERS_CAN_SAVE_FILE", True))
+  
   ABOUT_BOT_TEXT = f"""
  ʜᴇʀᴇ ᴡᴇ ɢᴏ ᴄʜᴇᴄᴋᴏᴜᴛ ᴍʏ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ✔
   
